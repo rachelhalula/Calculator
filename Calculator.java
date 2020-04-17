d2acb20497bccb47143dda346e98567e7a7b3588
rachelhalula <63815711+rachelhalula@users.noreply.github.com>
2020-04-17 19:02:31 -0400

    Added operator buttons

--- Calculator.java
+++ Calculator.java
@@ -17,6 +17,8 @@ public class Calculator
         cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         cFrame.setTitle("Simple Calculator");
         
+        initializeComponents();
+        
         cFrame.pack();
         cFrame.setVisible(true);
     }
@@ -26,7 +28,29 @@ public class Calculator
         return cFrame;
     }
     
-    
+    public void initializeComponents()
+    {
+        //the panel for the buttons to go on
+        JPanel jpButtons = new JPanel();
+        
+        //the buttons
+        JButton addButton = new JButton("+");
+        addButton.setName("addButton");
+        JButton subButton = new JButton("-");
+        subButton.setName("subButton");
+        JButton multButton = new JButton("*");
+        multButton.setName("multButton");
+        JButton divButton = new JButton("/");
+        divButton.setName("divButton");
+        
+        jpButtons.add(addButton);
+        jpButtons.add(subButton);
+        jpButtons.add(multButton);
+        jpButtons.add(divButton);
+        
+        cFrame.add(jpButtons, BorderLayout.PAGE_END);
+        
+    }
     
     public static void main(String[] args)
     {
