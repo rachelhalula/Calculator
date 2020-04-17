3c1ccdc85ec9d0bba3267b3f006f293ae1e82f9f
rachelhalula <63815711+rachelhalula@users.noreply.github.com>
2020-04-17 18:50:46 -0400

    Setting basics of gui

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,29 @@ import java.util.*;
 
 public class Calculator
 {
+    private Jframe cFrame;
+    private JLabel result;
+    
+    //Constructor
+    public Calculator()
+    {
+        cFrame = new JFrame();
+        cFrame.setLocation(100, 100);
+        cFrame.setSize(400, 400);
+        cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cFrame.setTitle("Simple Calculator");
+        
+        cFrame.pack();
+        cFrame.setVisible(true);
+    }
+    
+    public JFrame getFrame()
+    {
+        return cFrame;
+    }
+    
+    
+    
     public static void main(String[] args)
     {
         
